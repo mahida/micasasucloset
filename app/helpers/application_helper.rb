ddeb8eb24e061ffe560d9b9fe2a0b9bcbaf6578f
Courtney Mahida <mahida.guadalupe@gmail.com>
2016-02-06 11:47:50 -0500

    grouping devlopment and production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
+	
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		
 		if user.image
 			user.image
+		elsif user.avatar.url
+			user.avatar.url
 		else
 			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
 		end
 	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202183347) do
+ActiveRecord::Schema.define(version: 20151205215057) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -112,6 +112,10 @@ ActiveRecord::Schema.define(version: 20151202183347) do
     t.string   "image"
     t.string   "phone_number"
     t.text     "description"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
